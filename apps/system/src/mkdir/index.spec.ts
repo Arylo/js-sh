abf17e73fbd20533aa1527fbf698f6ba17d89e92
Arylo <arylo.open@gmail.com>
2023-09-14 10:43:14 +0800

    test(system): add unit test for mkdir

--- apps/system/src/mkdir/index.spec.ts
+++ apps/system/src/mkdir/index.spec.ts
@@ -5,9 +5,22 @@ import { mkdir } from './index'
 
 const test = mockPathTest()
 
-test('should make folder', (t) => {
+test('should make a folder', (t) => {
   const testPath = path.resolve(t.context.testPath, 'a/aa/aaa/aaaa')
-  mkdir(testPath)
+  t.true(mkdir(testPath))
   t.true(fs.existsSync(testPath))
   t.true(fs.statSync(testPath).isDirectory())
+})
+
+test('should make a existed folder', (t) => {
+  const testPath = path.resolve(t.context.testPath, 'a')
+  t.true(mkdir(testPath))
+  t.true(fs.existsSync(testPath))
+  t.true(fs.statSync(testPath).isDirectory())
+})
+
+test('should cannot make a folder but it is file', (t) => {
+  const testPath = path.resolve(t.context.testPath, 'a/aa.txt')
+  t.false(mkdir(testPath))
+  t.true(fs.statSync(testPath).isFile())
 })
